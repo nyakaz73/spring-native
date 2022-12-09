b37790c622780983a7aaaaf46d9d69a809111095
Tafadzwa <tafadzwalnyamukapa@gmail.com>
2022-12-09 10:09:01 +0300

    EnabledR2dbc Repositories

--- src/main/java/com/stackdev/springnative/SpringNativeApplication.java
+++ src/main/java/com/stackdev/springnative/SpringNativeApplication.java
@@ -2,8 +2,10 @@ package com.stackdev.springnative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class SpringNativeApplication {
 
 	public static void main(String[] args) {
